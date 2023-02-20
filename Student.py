12c2db9957dd3d8931530ed0a1f1dcdb4dca8343
qjrdpulga <qjrdpulga@tip.edu.ph>
2023-02-20 13:31:31 +0000

    Add Student.py

---
+++ Student.py
@@ -0,0 +1,14 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def __str__(self):
+        return f"{self.name}({self.age})"
+
+p1 = Student("Justin Pulga", 21)
+p2 = Student("Justin Robert", 22)
+
+print("Name: ", p1.name)
+print("Age: ", p1.age)
+print("Name: ", p2.name)
+print("Age: ", p2.age)
